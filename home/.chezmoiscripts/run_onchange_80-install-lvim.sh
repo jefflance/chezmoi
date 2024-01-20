5fe18c5cd3b4180b0951820bf14016a4070a53fb
Jeff LANCE <email@jefflance.me>
2024-01-20 22:50:27 +0100

    Up config.

--- home/.chezmoiscripts/run_onchange_80-install-lvim.sh
+++ home/.chezmoiscripts/run_onchange_80-install-lvim.sh
@@ -1,3 +1,15 @@
-#!/bin/env bash
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+
+if [ ! $(command -v nvim) ]; then
+  {{- if ne .chezmoi.username "root" }}
+    sudo yay -S --noconfirm neovim python-pynvim
+  {{- else }}
+    yay -S --noconfirm neovim python-pynvim
+  {{- end }}
+fi
+
 
 LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
