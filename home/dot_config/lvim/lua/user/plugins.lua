baf8f10ffad3ae25df77727f656ed0f93d171393
Jeff LANCE <email@jefflance.me>
2024-03-01 12:08:17 +0100

    Up.

--- home/dot_config/lvim/lua/user/plugins.lua
+++ home/dot_config/lvim/lua/user/plugins.lua
@@ -192,7 +192,7 @@ M.setup = function()
           },
           auto_update = true,
           run_on_start = true,
-          start_delay = 10000,
+          start_delay = 15000,
           debounce_hours = 24,
         })
       end
