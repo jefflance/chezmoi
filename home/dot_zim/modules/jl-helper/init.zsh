5d3a528052ceef71395289474225160c2a7a8e26
Jeff LANCE <email@jefflance.me>
2025-04-10 20:34:06 +0200

    Update home/.zim/modules/jl-helper/init.zsh

--- home/dot_zim/modules/jl-helper/init.zsh
+++ home/dot_zim/modules/jl-helper/init.zsh
@@ -30,7 +30,7 @@ alias neomutt='neomutt -f ${HOME}/Mail/jeff.lance@mala.fr/inbox/'
 #
 
 alias ip='ip -c'                         # ip address
-# alias cd='z'                             # use zoxide
+alias ls='eza --icons=always'            # ls aliased to eza, exa fork
 alias md='mkdir -p'                      # mkdir alias
 alias open='xdg-open'                    # open files
 alias pips='pip_search'                  # pip search tool
