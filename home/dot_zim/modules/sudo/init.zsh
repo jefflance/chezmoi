99bf84113027a82362c4ead51d869578ceddc988
Jeff LANCE <email@jefflance.me>
2025-06-21 19:26:25 +0200

    Update home/.zim/modules/sudo/init.zsh Update home/.zshenv

--- home/dot_zim/modules/sudo/init.zsh
+++ home/dot_zim/modules/sudo/init.zsh
@@ -1,6 +1,6 @@
 zle -N sudo-command-line
 
 # Defined shortcut keys: [Esc] [Esc]
-bindkey -M emacs '\e\e' sudo-command-line
-bindkey -M vicmd '\e\e' sudo-command-line
-bindkey -M viins '\e\e' sudo-command-line
+bindkey -M emacs '\es' sudo-command-line
+bindkey -M vicmd '\es' sudo-command-line
+bindkey -M viins '\es' sudo-command-line
