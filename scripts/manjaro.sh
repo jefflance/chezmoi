3c58a3f0a4c8d322ac911b45e36673e73a1f1294
Jeff Lance <jeff.lance@mala.fr>
2025-08-01 01:51:00 +0200

    up

--- scripts/manjaro.sh
+++ scripts/manjaro.sh
@@ -145,7 +145,9 @@ main() {
   "$ZSH" && install_zsh
   "$LATEX" && install_latex
   "$VSCODE" && install_vscode
-  "$QUARTO" && install_quarto && configure_quarto
+  if [ "$QUARTO" && install_quarto ]; then
+      configure_quarto
+  fi
 
   ## Install yay
   if [ ! $(command -v yay) ]; then
