53d54db0c89155141b1310c83a2d4a214e8bd550
Jeff LANCE <email@jefflance.me>
2026-04-08 21:38:44 +0200

    Update .config/spaceship.zsh

--- home/dot_config/empty_spaceship.zsh
+++ home/dot_config/empty_spaceship.zsh
@@ -13,4 +13,4 @@ SPACESHIP_DIR_TRUNC_REPO=false
 
 # Add a custom vi-mode section to the prompt
 # See: https://github.com/spaceship-prompt/spaceship-vi-mode
-spaceship add --before char vi_mode
+# spaceship add --before char vi_mode
