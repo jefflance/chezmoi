344fcdc1676e84e4a84838999574f94082af6e32
Jeff LANCE <email@jefflance.me>
2026-07-14 16:30:31 +0200

    Update .zim/modules/jl-helper/init.zsh

--- home/dot_zim/modules/jl-helper/init.zsh
+++ home/dot_zim/modules/jl-helper/init.zsh
@@ -29,9 +29,23 @@ alias neomutt='neomutt -f ${HOME}/Mail/jeff.lance@mala.fr/inbox/'
 # Utilities
 #
 
+if [[ "$TERM" == 'xterm-kitty' ]]; then
+  ## kssh
+  # Use this when your terminfo isn't recognized on remote hosts.
+  # See: https://sw.kovidgoyal.net/kitty/faq/#i-get-errors-about-the-terminal-being-unknown-or-opening-the-terminal-failing-when-sshing-into-a-different-computer
+  alias -g kssh="kitty +kitten ssh"
+  compdef kssh='ssh'
+  # Use this if kssh fails
+  alias kssh-slow="infocmp -a xterm-kitty | ssh myserver tic -x -o \~/.terminfo /dev/stdin"
+
+  # Change the colour theme
+  alias kitty-theme="kitty +kitten themes"
+fi
+
 [[ $(command -v trash) ]] && alias rm='trash-put'
 [[ $(command -v dfc) ]] && alias df='dfc'
 [[ $(command -v eza) ]] && alias ls='eza -l --icons=always'
+
 alias ip='ip -c'                         # ip address
 alias md='mkdir -p'                      # mkdir alias
 alias open='xdg-open'                    # open files
